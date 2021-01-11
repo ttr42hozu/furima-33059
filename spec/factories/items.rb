2c7f94bf6363ad2b4cf33e2ba6e6409fc8c53086
ttr42hozu <shigi10nato@gmail.com>
2021-01-11 20:57:21 +0900

    FactoryBot修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :item do
-    image { Faker::File.file_name }
     title { Faker::Games::Pokemon.name}
     description { Faker::Games::Pokemon.name}
     category_id { Faker::Number.within(range: 2..11)}
@@ -9,6 +8,9 @@ FactoryBot.define do
     ship_to_area_id { Faker::Number.within(range: 2..48)}
     ship_to_day_id { Faker::Number.within(range: 2..4)}
     price { Faker::Number.within(range: 300..9999999)}
-    user_id { Faker::Number.within(range: 1..9999999)}
+    association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
