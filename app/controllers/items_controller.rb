759855996c57dd739264333a3c640e8b73d0b134
ttr42hozu <shigi10nato@gmail.com>
2021-01-12 11:24:03 +0900

    itemsコントローラーの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,18 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item= Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+  private
+  def item_params
+    params.require(:item).permit(:title, :description, :category_id, :status_id,:shipping_charge_id, :ship_to_area_id, :ship_to_day_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
+
