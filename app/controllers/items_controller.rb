e1ce6c20307f388365da3b377b9643fc8b127423
ttr42hozu <shigi10nato@gmail.com>
2021-01-13 19:12:57 +0900

    updateアクションの定義とbeforeアクション(2つ ）の定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: :edit
+  before_action :move_to_index, only: :edit
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -26,10 +28,28 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render action: :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:title, :description, :category_id, :status_id, :shipping_charge_id, :ship_to_area_id,
                                  :ship_to_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  def move_to_index
+    unless current_user.id == @item.user.id
+      redirect_to action: :index
+    end
   end
 end
