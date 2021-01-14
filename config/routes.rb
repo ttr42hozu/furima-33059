0316d6f35a3764dd36aa798624b4deece86437a8
ttr42hozu <shigi10nato@gmail.com>
2021-01-14 19:04:47 +0900

    修正（ネスト忘れ）

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items
+  resources :items do
+    resources :orders, only: :index
+  end
 
-  resources :orders, only: :index
 end
 
