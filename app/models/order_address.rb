03df4d251468179d14df717ad9a2264f103d6e72
ttr42hozu <shigi10nato@gmail.com>
2021-01-18 11:18:54 +0900

    orderaddressの定義とバリデーションの移動

---
+++ app/models/order_address.rb
@@ -0,0 +1,17 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postcode, :ship_to_area_id, :city, :block, :building, :phone_number
+
+  with_options presence: true do
+    validates :postcode, format: { with: /\A\d{3}[-]\d{4}\z/}
+    validates :ship_to_area_id, numericality: { other_than: 1 }
+    validates :city
+    validates :block
+    validates :phone_number, format: { with: /\A\d{11}\z/}
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    @order_address = Address.create(postcode: postcode, ship_to_area_id: ship_to_area_id, city: city, block: block, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
