66819fb4c478a5408f8f84e33ecef29ec364a3a7
ttr42hozu <shigi10nato@gmail.com>
2021-01-19 11:42:13 +0900

    order_addressモデルの記述

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postcode, :ship_to_area_id, :city, :block, :building, :phone_number
+  attr_accessor :user_id, :item_id, :postcode, :ship_to_area_id, :city, :block, :building, :phone_number, :token
 
   with_options presence: true do
     validates :postcode, format: { with: /\A\d{3}[-]\d{4}\z/}
@@ -8,6 +8,7 @@ class OrderAddress
     validates :city
     validates :block
     validates :phone_number, format: { with: /\A\d{11}\z/}
+    validates :token
   end
 
   def save
